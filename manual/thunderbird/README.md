590256dd4f7d42d991c974ed3d107e9f49faba3f
dotverx <259408855+dotverx@users.noreply.github.com>
2026-02-05 22:58:18 +0100

    Add thunderbird setup

---
+++ manual/thunderbird/README.md
@@ -0,0 +1,35 @@
+# Betterbird setup
+
+## Version A: create a new profile
+### Step 1
+
+Run:
+```
+betterbird -P
+```
+
+Create a new profile named `main` and as the directory for the profile choose `~/.thunderbird/main`.
+
+#### Option A: you already have a betterbird profile
+
+Inside `~/.thunderbird/main` backup existing profile data `~/.thunderbird/PREVIOUSPROFILE` and `profiles.ini`.
+Fill the `~/.thunderbird/main` (created with `betterbird -P`) with data from the previous profile.
+```
+cp -a ~/.thunderbird/PREVIOUSPROFILE ~/.thunderbird/main
+```
+
+Use `profiles.ini` for reference to create a new `~/.thunderbird/profiles.ini`.
+
+#### Option B: you don't have a betterbird profile
+
+Copy `profiles.ini` to `~/.thunderbird.profiles.ini`.
+
+### Step 2
+
+Move `user.js` to `~/.thunderbird/main/user.js`.
+
+## Version B: use existing profile
+
+### Step 1
+
+Move `user.js` to `~/.thunderbird/EXISTINGPROFILE/user.js`.
---
+++ manual/thunderbird/user.js
@@ -0,0 +1,37 @@
+// Mozilla User Preferences
+// betterbird
+
+user_pref("mail.uidensity", 0);
+user_pref("mail.uifontsize", 13);
+
+// GENERAL
+user_pref("mailnews.start_page.enabled", false);
+user_pref("mail.shell.checkDefaultClient", false);
+user_pref("mail.minimizeToTray", true);
+user_pref("font.name.sans-serif.x-western", "sans-serif");
+user_pref("font.size.variable.x-western", 14);
+user_pref("font.size.monospace.x-western", 14);
+user_pref("layout.css.always_underline_links", true);
+user_pref("offline.download.download_messages", 1);
+user_pref("offline.startup_state", 4);
+user_pref("mail.prompt_purge_threshold", false);
+user_pref("mail.fixed_width_messages", false);
+
+// APPEARANCE
+user_pref("mail.tabs.drawInTitlebar", false);
+user_pref("mailnews.default_view_flags", 1);
+
+// COMPOSITION
+user_pref("spellchecker.dictionary", "en-US,pl");
+user_pref("mail.compose.default_to_paragraph", false);
+user_pref("mail.compose.big_attachments.notify", false);
+
+// PRIVACY & SECURITY
+user_pref("places.history.enabled", false);
+user_pref("network.cookie.cookieBehavior", 2);
+
+// CHAT
+user_pref("messenger.startup.action", 0);
+user_pref("messenger.status.reportIdle", false);
+user_pref("purple.conversations.im.send_typing", false);
+user_pref("messenger.options.messagesStyle.theme", "simple");
