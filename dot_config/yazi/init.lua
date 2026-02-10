35637f81c00f3e0aef721aa6506d6c9867c72207
dotverx <259408855+dotverx@users.noreply.github.com>
2026-02-10 20:05:32 +0100

    Add yazi plugins

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -30,3 +30,35 @@ function Linemode:mdate_perm_own()
   return string.format("%s %s %s:%s", mtime, perm, owner, group)
 end
 
+-- git.yazi plugin
+require("git"):setup {
+  -- order of status signs showing in the linemode
+  order = 900,
+}
+
+require("eza-preview"):setup({
+  -- Set the tree preview to be default (default: true)
+  default_tree = false,
+  -- Directory depth level for tree preview (default: 3)
+  level = 3,
+  -- Show file icons
+  icons = true,
+  -- Follow symlinks when previewing directories (default: true)
+  follow_symlinks = true,
+  -- Show target file info instead of symlink info (default: false)
+  dereference = false,
+  -- Show hidden files (default: true)
+  all = true,
+  -- Ignore files matching patterns (default: {})
+  -- ignore_glob = "*.log"
+  -- ignore_glob = { "*.tmp", "node_modules", ".git", ".DS_Store" }
+  -- SEE: https://www.linuxjournal.com/content/pattern-matching-bash to learn about glob patterns
+  ignore_glob = {},
+  -- Ignore files mentioned in '.gitignore'  (default: true)
+  git_ignore = false,
+  -- Show git status (default: false)
+  git_status = false,
+})
+
+require("recycle-bin"):setup()
+
